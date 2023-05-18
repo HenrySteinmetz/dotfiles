7d8f120c98bff8de04995eaede912547a9b9f9ac
Henry <matteo.steinmetz@t-online.de>
2023-05-18 14:48:55 +0200

    fixed zsh

--- postinstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd 
-cd .zsh/
-git clone https://github.com/agkozak/zsh-z.git
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-git https://github.com/zsh-users/zsh-autosuggestions.git
-cd
